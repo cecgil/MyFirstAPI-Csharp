09e5a5bfc64d55a352679cbf8cb2200c1f683348
cecgil <dudigil6@gmail.com>
2025-09-09 23:01:05 -0300

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("new API");
+    }
+}
