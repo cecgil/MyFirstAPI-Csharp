98d6f85bc5eaee9695ea944c5d20abff88aa583a
cecgil <dudigil6@gmail.com>
2025-09-09 23:15:41 -0300

    Feature: Criação rota post juntamente com responses, requests e organização da estrutura de pastas.

---
+++ Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstAPI.Communication.Responses;
+
+public class ResponseRegisterUserJson
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Communication.Requests;
+using MyFirstAPI.Communication.Responses;
 
 namespace MyFirstAPI.Controllers;
 
@@ -7,8 +9,33 @@ namespace MyFirstAPI.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get()
+    [Route("{id}")]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult GetById([FromRoute] int id)
     {
-        return Ok("new API");
+        var response = new User
+        {
+            Id = id,
+            Name = "John Doe",
+            Age = 30
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(RequestRegisterUserJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult Create([FromBody] RequestRegisterUserJson requestRegisterUserJson)
+    {
+        var response = new ResponseRegisterUserJson
+        {
+            Id = new Random().Next(1, 1000), // Simulate ID generation
+            Name = requestRegisterUserJson.Name,
+        };
+
+        // In a real application, you would save the user to a database here.
+        return Created(string.Empty, response);
     }
 }
