aea72fd7e891f1072a127b72e92c7928b2ac1dcc
cecgil <dudigil6@gmail.com>
2025-09-17 23:32:09 -0300

    Criação completa do CRUD com endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -38,4 +38,46 @@ public class UserController : ControllerBase
         // In a real application, you would save the user to a database here.
         return Created(string.Empty, response);
     }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestUpdateUserProfileJson request)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        var response = new List<User>()
+        {
+            new User {Id = 1, Age = 7, Name = "welisson"},
+            new User {Id = 2, Age = 7, Name = "maria"}
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPut("change-password")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
+    {
+        return NoContent();
+    }
+
+    [HttpPut("change-welisson")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult ChangePassword2([FromBody] RequestChangePasswordJson request)
+    {
+        return NoContent();
+    }
 }
